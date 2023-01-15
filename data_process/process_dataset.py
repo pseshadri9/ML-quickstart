138092f911bfeb609b373ce3c949055453bab744
pseshadri9 <pavan.seshadri1@gmail.com>
2023-01-15 00:21:10 -0500

    pl dataset and model

--- data_process/process_dataset.py
+++ data_process/process_dataset.py
@@ -1,32 +1,36 @@
+import pytorch_lightning as pl
+from torch.utils.data import DataLoader 
 
-'''
-Load data from file and return pandas/np array
-containing unaltered data
-'''
-def load_data(filepath):
-    pass
-
-'''
-Process loaded dataset into desirable format/etc.
-'''
-def process_data(data):
-    pass
-
-'''
-Partition data into desired samples, etc. 
-for train/test/val
-'''
-def partition_data(data):
-    pass
-
-
-'''
-wrapper function to return usable dataset from filepath
-'''
-
-def create_dataset(filepath):
-    data = load_data(filepath)
-    data = process_data(data)
-
-    return data
-
+class DataModule(pl.LightningDataModule):
+    def __init__(self, filepath, batch_size):
+        super().__init__()
+          
+        self.filepath = filepath
+          
+        self.batch_size = batch_size
+  
+    def prepare_data(self):
+        pass
+  
+    def setup(self, stage=None):
+        
+        #Load and format dataset
+        self.train_data, self.val_data, self.test_data = None
+  
+    def train_dataloader(self):
+        
+          # Generating train_dataloader
+        return DataLoader(self.train_data, 
+                          batch_size = self.batch_size)
+  
+    def val_dataloader(self):
+        
+          # Generating val_dataloader
+        return DataLoader(self.val_data,
+                          batch_size = self.batch_size)
+  
+    def test_dataloader(self):
+        
+        # Generating test_dataloader
+        return DataLoader(self.test_data,
+                          batch_size = self.batch_size)
