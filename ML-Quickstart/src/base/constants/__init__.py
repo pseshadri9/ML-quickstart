a58165ac219a38a9381eda8dd13ef62ee025ce69
pseshadri9 <pavan.seshadri1@gmail.com>
2024-12-13 19:42:06 -0500

    mspec params for pitch shift

--- ML-Quickstart/src/base/constants/__init__.py
+++ ML-Quickstart/src/base/constants/__init__.py
@@ -19,3 +19,17 @@ SAMPLE_RATE = 16000
 BATCH_SIZE = 16
 NUM_WORKERS = os.cpu_count() // 2
 DEVICE = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+FRAMES_PER_SECOND = 100
+
+# Mel spectrogram parameters
+MSPEC_PARAMS = dict(
+    n_fft=2048,
+    win_length=2048,
+    hop_length=SAMPLE_RATE // FRAMES_PER_SECOND,
+    n_mels=229,
+    f_min=30,
+    f_max=SAMPLE_RATE // 2,
+    center=True,
+    pad_mode="reflect",
+)
